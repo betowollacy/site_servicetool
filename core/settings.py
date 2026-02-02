5f3b127012e312ea56da160bb156448aac1556fb
betowollacy <betowollacy@gmail.com>
2026-02-01 21:12:32 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # destino final
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),                # pasta de origem
 ]
